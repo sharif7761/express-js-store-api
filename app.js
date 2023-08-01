417c2d9932913c78dc4a377c0153a719193df047
ahmed7761 <sharif7761@gmail.com>
2023-08-02 00:12:21 +0600

    setup product route

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+const productRouters = require('./routes/products')
+
 const  notFoundMiddleware = require('./middleware/not-found');
 const  errorMiddleware = require('./middleware/error-handler');
 
@@ -14,12 +17,15 @@ app.get('/', (req, res) => {
     res.send('<h1>Store Api</h1><a href="/api/v1/products">Products</a>')
 })
 
+app.use('/api/v1/products', productRouters)
+
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
 const port = process.env.PORT || 3000
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, console.log(`Server is listening port ${port}...`))
     }
     catch (error){
---
+++ controllers/products.js
@@ -0,0 +1,8 @@
+
+const getAllProducts = async (req, res) => {
+    res.status(200).json({msg: 'products testing route'})
+}
+
+module.exports = {
+    getAllProducts
+}
