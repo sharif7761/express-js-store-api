358ed79c0914744a0d25dcc7c4c41afd71e6d8e0
ahmed7761 <sharif7761@gmail.com>
2023-08-07 00:54:09 +0600

    added name search query with regex

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,23 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-    console.log(req.query)
     // use all query
    // const products = await Product.find(req.query)
 
     // avoid unnecessary search data
-    const { featured } = req.query
+    const { featured, company, name } = req.query
 
     const queryObject = {}
     if(featured){
         queryObject.featured = featured === 'true' ? true : false;
+    }
+
+    if(company){
+        queryObject.company = company;
+    }
+
+    if(name){
+        queryObject.name = { $regex: name, $options: 'i'};
     }
     const products = await Product.find(queryObject)
     res.status(200).json({products, nbHits: products.length })
