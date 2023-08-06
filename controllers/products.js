c3369c6bdb6941edd6b6bd2d9774affe2961ef15
ahmed7761 <sharif7761@gmail.com>
2023-08-07 00:23:27 +0600

    product search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,9 @@
+const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products testing route'})
+    console.log(req.query)
+    const products = await Product.find(req.query)
+    res.status(200).json({products, nbHits: products.length })
 }
 
 module.exports = {
