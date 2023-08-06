c5f8040f502fb9945dfbf5070903323980fb076d
ahmed7761 <sharif7761@gmail.com>
2023-08-07 00:33:56 +0600

    refactor product search query

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,17 @@ const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
     console.log(req.query)
-    const products = await Product.find(req.query)
+    // use all query
+   // const products = await Product.find(req.query)
+
+    // avoid unnecessary search data
+    const { featured } = req.query
+
+    const queryObject = {}
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    const products = await Product.find(queryObject)
     res.status(200).json({products, nbHits: products.length })
 }
 
