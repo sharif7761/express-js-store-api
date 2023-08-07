7c2a0c200452a9c76e65cd91d1bc004b84e1da03
ahmed7761 <sharif7761@gmail.com>
2023-08-08 00:01:41 +0600

    added select functionality

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,7 @@ const getAllProducts = async (req, res) => {
    // const products = await Product.find(req.query)
 
     // avoid unnecessary search data
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields } = req.query
 
     const queryObject = {}
     if(featured){
@@ -28,6 +28,12 @@ const getAllProducts = async (req, res) => {
     } else {
         result = result.sort('createdAt')
     }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
     const products = await result
     res.status(200).json({products, nbHits: products.length })
 }
