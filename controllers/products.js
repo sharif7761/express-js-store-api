598f1c20d3db854e0445db15c356edf2474a2973
ahmed7761 <sharif7761@gmail.com>
2023-08-09 00:01:20 +0600

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,7 @@ const getAllProducts = async (req, res) => {
    // const products = await Product.find(req.query)
 
     // avoid unnecessary search data
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
 
     const queryObject = {}
     if(featured){
@@ -18,6 +18,26 @@ const getAllProducts = async (req, res) => {
 
     if(name){
         queryObject.name = { $regex: name, $options: 'i'};
+    }
+
+    if(numericFilters){
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|>=|=|<=)\b/g
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator,value] = item.split('-')
+            if(options.includes(field)){
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        })
     }
 
 
